9e1f74730fc598095417558e8afb75a273a74c15
Messi002 <rostandmessi2@gmail.com>
2024-04-13 14:30:00 +0100

    added keys

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -5,7 +5,7 @@ const connection = {};
 export const connectToDB = async () => {
   try {
     if (connection.isConnected) return;
-    const db = await mongoose.connect("mongodb+srv://rostandmessi2:8nEnf2Tb7zUEeFn4@cluster0.wu9wrfx.mongodb.net/dashboard?retryWrites=true&w=majority&appName=Cluster0");
+    const db = await mongoose.connect(process.env.MONGO);
     connection.isConnected = db.connections[0].readyState;
   } catch (error) {
     console.log(error)
