3b0f6279424b7c2e7182ab26bad15444d120cf9d
Messi002 <rostandmessi2@gmail.com>
2024-04-15 23:17:52 +0100

    final updates

--- middleware.js
+++ middleware.js
@@ -6,3 +6,27 @@ export default NextAuth(authConfig).auth;
 export const config = {
   matcher: ['/((?!api|static|.*\\..*|_next).*)'],
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MONGO = "mongodb+srv://rostandmessi2:8nEnf2Tb7zUEeFn4@cluster0.wu9wrfx.mongodb.net/dashboard?retryWrites=true&w=majority&appName=Cluster0"
+// AUTH_SECRET = TNfLQgBJUc60zY0WWHI2TdNKqxKIEIttmMHmMUz49t8=
+// AUTH_URL = http://localhost:3000/api/auth
